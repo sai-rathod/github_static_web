79362c60c9efdb5fba7ff576c1e63fc939c5eda5
Sayindra Rathod <sahilrathodrk31@gmail.com>
2025-10-28 17:09:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
 document.getElementById("clickBtn").addEventListener("click", () => {
-  const msg = document.getElementById("message");
-  msg.textContent = "🚀 Static Website Successfully Hosted on GitHub Pages!";
-  msg.classList.add("show");
+  window.open("https://elevatelabs.in/", "_blank");
 });
